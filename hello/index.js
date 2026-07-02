1fa188e856d76dae7146609392a14b4d04084e10
Manikandan G <158708882+MANIKANDAN242221@users.noreply.github.com>
2026-07-02 15:12:32 +0530

    Delete hello directory

--- hello/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello world');
-});
-
-server.listen(8001, () => {
-  console.log('Hello service listening on port 8001');
-});
