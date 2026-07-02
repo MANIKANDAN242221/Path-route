7134f7254f59235f2d19682a25c97722bdf6644c
MANIKANDAN242221 <manikandanpro2020@gmail.com>
2026-07-02 15:24:05 +0530

    update

--- hi/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hi everyone');
-});
-
-server.listen(8002, () => {
-  console.log('Hi service listening on port 8002');
-});
